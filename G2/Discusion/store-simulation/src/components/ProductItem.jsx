c112258eee598070bb296272c133f405698d3271
karivas-dev <karenrivasg17@gmail.com>
2023-08-21 23:45:31 -0600

    Fix classname

--- G2/Discusion/store-simulation/src/components/ProductItem.jsx
+++ G2/Discusion/store-simulation/src/components/ProductItem.jsx
@@ -2,15 +2,15 @@ import React from "react";
 const ProductItem = ({ id, name, price, image }) => {
   return (
     <div className="col-4 my-4">
-      <div class="card text-center">
-        <div class="card-header"><b>Product id:</b> {id}</div>
-        <img src={image} class="card-img-top" alt="Product image"/>
-        <div class="card-body">
-          <h5 class="card-title">{name}</h5>
-          <p class="card-text">
+      <div className="card text-center">
+        <div className="card-header"><b>Product id:</b> {id}</div>
+        <img src={image} className="card-img-top" alt="Product image"/>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
             {price}
           </p>
-          <a href="#" class="btn btn-primary">
+          <a href="#" className="btn btn-primary">
             Buy it now
           </a>
         </div>
