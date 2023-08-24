57850eceebae514565bea61d6e2bf5a61b4399ff
Karen Rivas <karenrivasg17@gmail.com>
2023-08-23 18:15:31 -0600

    Implement G3

---
+++ G3/cart-react/src/components/Headers.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+export const Header = ({
+    allProducts,
+    setAllProducts,
+    total,
+    countProducts,
+    setCountProducts,
+    setTotal,
+}) => {
+    const [active, setActive] = useState(false);
+    const onDeleteProduct = product => {
+        const results = allProducts.filter(
+            item => item.id !== product.id
+        );
+        setTotal(total - product.price * product.quantity);
+        setCountProducts(countProducts - product.quantity);
+        setAllProducts(results);
+    };
+    const onCleanCart = () => {
+        setAllProducts([]);
+        setTotal(0);
+        setCountProducts(0);
+    };
+    return (
+        <header>
+            <h1>Tienda de Libros</h1>
+            <div className='container-icon'>
+                <div
+                    className='container-cart-icon'
+                    onClick={() => setActive(!active)}
+                >
+                    <i class="bi bi-cart-fill icon-cart"></i>
+                    <div className='count-products'>
+                        <span id='contador-productos'>{countProducts}</span>
+                    </div>
+                </div>
+                <div
+                    className={`container-cart-products ${active ? '' : 'hidden-cart'
+                        }`}
+                >
+                    {allProducts.length ? (
+                        <>
+                            <div className='row-product'>
+                                {allProducts.map(product => (
+                                    <div className='cart-product'
+                                        key={product.id}>
+                                        <div className='info-cart-product'>
+                                            <span className='cantidad-productocarrito'>
+                                                {product.quantity}
+                                            </span>
+                                            <p className='titulo-productocarrito'>
+                                                {product.title}
+                                            </p>
+                                            <span className='precio-productocarrito'>
+                                                ${product.price}
+                                            </span>
+                                        </div>
+                                        <img
+                                            src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                                            alt="cerrar"
+                                            className="icon-close"
+                                            onClick={() => onDeleteProduct(product)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='cart-total'>
+                                <h3>Total:</h3>
+                                <span className='total-pagar'>${total}</span>
+                            </div>
+                            <button className='btn-clear-all'
+                                onClick={onCleanCart}>
+                                Vaciar Carrito
+                            </button>
+                        </>
+                    ) : (
+                        <p className='cart-empty'>El carrito está vacío</p>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+};
---
+++ G3/cart-react/src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Cuentos de Barro',
+        price: 30,
+        urlImage: 'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Tierra de Infancia',
+        price: 30,
+        urlImage: 'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+        quantity: 1
+    }, 
+    {
+        id: 5,
+        title: 'Harry Potter Pack',
+        price: 390,
+        urlImage: 'https://m.media-amazon.com/images/I/81GlkLlEKpL._AC_UF1000,1000_QL80_.jpg',
+        quantity: 1
+    }
+];
