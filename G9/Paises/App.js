eb928cfba51e7706a63ff7336cb54560f50ef57d
karivas-dev <karenrivasg17@gmail.com>
2023-10-19 00:12:03 -0600

    Complementario: Image not available

--- G9/Paises/App.js
+++ G9/Paises/App.js
@@ -1,60 +1,61 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Alert } from "react-native";
-import React, { useState, useEffect } from "react";
+import {StatusBar} from "expo-status-bar";
+import {Alert, StyleSheet, View} from "react-native";
+import React, {useEffect, useState} from "react";
 import Formulario from "./src/components/Formulario";
 import Pais from "./src/components/Pais";
 
 export default function App() {
-  const [busqueda, guardarBusqueda] = useState({ pais: "" });
-  const [consultar, guardarConsulta] = useState(false);
-  const [resultado, guardarResultado] = useState({});
+    const [busqueda, guardarBusqueda] = useState({pais: ""});
+    const [consultar, guardarConsulta] = useState(false);
+    const [resultado, guardarResultado] = useState({});
 
-  useEffect( () => {
-    const { pais } = busqueda;
-    const consultarPais = async () => {
-      if (consultar) {
-        const url = `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`
+    useEffect(() => {
+        const {pais} = busqueda;
+        const consultarPais = async () => {
+            if (consultar) {
+                const url = `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`
 
-        try {
-          const respuesta = await fetch(url);
-          const resultado = await respuesta.json();
+                try {
+                    const respuesta = await fetch(url);
+                    const resultado = await respuesta.json();
 
-          guardarResultado(resultado);
-          guardarConsulta(false);
-        } catch ( error ) {
-          mostrarAlerta();
+                    guardarResultado(resultado);
+                    guardarConsulta(false);
+                } catch (error) {
+                    mostrarAlerta();
+                }
+            }
         }
-      }
-    }
 
-    consultarPais();
- }, [consultar]);
+        consultarPais();
+    }, [consultar]);
 
- const mostrarAlerta = () => {
-  Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'), [{ Text: 'Ok'}];
- };
+    const mostrarAlerta = () => {
+        Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'), [{Text: 'Ok'}];
+    };
 
-  return (
-    <View style={styles.app}>
-      <View style={styles.contenido}>
-        <Formulario 
-        busqueda={busqueda} 
-        guardarbusqueda={guardarBusqueda}
-        guardarconsulta={guardarConsulta} />
-        <Pais resultado={resultado}/>
-        <StatusBar style="auto" />
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.app}>
+            <View style={styles.contenido}>
+                <Formulario
+                    busqueda={busqueda}
+                    guardarbusqueda={guardarBusqueda}
+                    guardarconsulta={guardarConsulta}
+                />
+                <Pais resultado={ resultado } shortPais={ busqueda.pais }/>
+                <StatusBar style="auto"/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-    backgroundColor: 'rgb(71,149,212)',
-    justifyContent: "center",
-  },
-  contenido: {
-    margin: '2.5%',
-  }
+    app: {
+        flex: 1,
+        backgroundColor: 'rgb(71,149,212)',
+        justifyContent: "center",
+    },
+    contenido: {
+        margin: '2.5%',
+    }
 });
--- G9/Paises/src/components/Pais.js
+++ G9/Paises/src/components/Pais.js
@@ -1,21 +1,25 @@
 import React, {useEffect, useState} from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { Card } from "react-native-elements";
 
-const Pais = ({ resultado }) => {
+const Pais = ({ resultado, shortPais }) => {
     const [info, setInfo] = useState([]);
     const [nombre, setNombre] = useState();
     const [capital, setCapital] = useState();
     const [region, setRegion] = useState();
     const [lengua, setLengua] = useState([]);
+    const [area, setArea] = useState();
+    const [pais, setPais] = useState(null);
 
     useEffect(() => {
         setInfo(resultado);
         lengua.length = 0;
+        setPais(shortPais);
         Object.values(resultado).map((e) => {
             setNombre(e.nome.abreviado);
             setCapital(e.governo.capital.nome);
             setRegion(e.localizacao.regiao.nome);
+            setArea(e.area.total + " " + e.area.unidade.símbolo);
 
             Object.values(e.linguas).map((l) => {
                 lengua.push(l.nome);
@@ -28,9 +32,11 @@ const Pais = ({ resultado }) => {
             <Card.Title>{ nombre }</Card.Title>
             <Card.Divider/>
             <View style={{ justifyContent: "center" }}>
+                <Image source={`https://flagcdn.com/16x12/${pais}.png`}/>
                 <Text>Capital: { capital }</Text>
-                <Text>Región: { region } </Text>
+                <Text>Región: { region }</Text>
                 <Text>Lengua: { lengua.toString() }</Text>
+                <Text>Área: { area }</Text>
             </View>
         </Card>
     );
